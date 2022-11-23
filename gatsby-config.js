9dbd424261cd5d20e9f54ceb1114a1e8b3769cdf
heimoshuiyu <heimoshuiyu@gmail.com>
2022-11-24 00:56:35 +0800

    add progress bar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,33 @@
 module.exports = {
   siteMetadata: {
     title: `Social OjO`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         name: `pages`,
         path: `${__dirname}/src`,
       },
     },
     {
-      resolve: 'gatsby-plugin-google-gtag',
+      resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingIds: [
-          "G-TVTHN7455T",
-        ],
+        trackingIds: ["G-TVTHN7455T"],
       },
     },
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-  ]
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: true,
+      },
+    },
+  ],
 };
